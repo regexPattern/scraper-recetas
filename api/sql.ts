3f03f065b99eaa6d10e366a820dc677debf4cc6b
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-10-01 16:00:57 -0300

    Finalizada reimplementacion en Vanilla TS
    Todavia falta agregar otros sitios de recetas e implementar el
    serializer para las queries SQL de las recetas.

---
+++ api/sql.ts
@@ -0,0 +1,12 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import type { Receta } from "./_scrapers.js";
+import handler from "./_handler.js";
+
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  handler(request, response, (receta: Receta) => {
+    return `INSERT INTO ... ${JSON.stringify(receta)}`;
+  });
+}
